183f5ebcfac6f37b7358ce9e9ef6c0b5e6efaa4b
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-04 21:38:53 -0600

    Navegacion entre componentes

--- practicas/react-router-tutorial/components/Navbar.jsx
+++ practicas/react-router-tutorial/components/Navbar.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
     <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Aplicacion Enrutamiento</a>
+    <Link className="navbar-brand" href="#">Aplicacion Enrutamiento</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <NavLink to='/' className="nav-link active" aria-current="page" >Home</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
+          <NavLink to='/contact' className="nav-link" >Contact</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+          <NavLink to='/about' className="nav-link" >About Me</NavLink>
         </li>
       </ul>
     </div>
--- practicas/react-router-tutorial/routes/AboutScreen.jsx
+++ practicas/react-router-tutorial/routes/AboutScreen.jsx
@@ -1,6 +1,13 @@
 export const AboutScreen = () => {
   return (
-    <div>AboutScreen</div>
+    <>
+    <select class="form-select" multiple aria-label="Multiple select example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+    </>
   )
 }
 
