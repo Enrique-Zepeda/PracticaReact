8412ea802fd4361f046279627bcee606a90b9db2
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-04 21:26:26 -0600

    App de Enrutamiento

---
+++ practicas/react-router-tutorial/src/App.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Navbar } from "../components/Navbar"
+import { HomeScreen } from "../routes/HomeScreen"
+import { AboutScreen } from "../routes/AboutScreen"
+import { ContactScreen } from "../routes/ContactScreen"
+
+export const App = () => {
+  return (
+    <>
+    <Navbar></Navbar>
+
+    <Routes>
+      <Route path="/" element={ <HomeScreen></HomeScreen> }></Route>
+      <Route path="/about" element={ <AboutScreen></AboutScreen> }></Route>
+      <Route path="/contact" element={ <ContactScreen></ContactScreen> }></Route>
+      <Route path="/*" element={ <Navigate to ='/'></Navigate> }></Route>
+    </Routes>
+    </>
+  )
+}
