4c576c2cf352934f9f6377133ff5a54c66e6ab4a
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-07 13:42:47 -0600

    Errores arreglados

--- practicas/Carrito De Compras/src/context/CarritoProvider.jsx
+++ practicas/Carrito De Compras/src/context/CarritoProvider.jsx
@@ -1,63 +1,49 @@
-import { useReducer } from "react"
-import { CarritoContext } from "./CarritoContext"
+import { useReducer } from "react";
+import { CarritoContext } from "./CarritoContext";
 
-const initialState = []
+const initialState = [];
 
-export const CarritoProvider = ({{children}}) => {
+const comprasReducer = (state, action) => {
+    switch (action.type) {
+        case '[CARRITO] Agregar Compra':
+            return [...state, action.payload];
 
-    const [listaCompras, dispatch] = useReducer(comprasReducer, initialState)
+        case '[CARRITO] Aumentar Cantidad':
+            // Logic for increasing quantity
+            break;
+        case '[CARRITO] Disminuir Cantidad':
+            // Logic for decreasing quantity
+            break;
+        case '[CARRITO] Eliminar Compra':
+            return state.filter(compra => compra.id !== action.payload);
 
-    const agregarCompra = (compra) => {
-        const action = {
-            type: '[CARRITO] Agregar Compra',
-            payload: compra
-        }
-        dispatch(action)
+        default:
+            return state;
     }
-    const aumentarCantidad = (id) => {
-        const action = {
-            type: '[CARRITO] Aumentar Cantidad',
-            payload: id
-        }
-        dispatch(action)
-    }
-    const disminuirCantidad = (id) => {
-        const action = {
-            type: '[CARRITO] Disminuir Cantidad',
-            payload: id
-        }
-        dispatch(action)
-    }
-    const eliminarCompra= (id) => {
-        const action = {
-            type: '[CARRITO] Eliminar Compra',
-            payload: id
-        }
-        dispatch(action)
-    }
+};
 
-    const comprasReducer = (state = initialState, action = {}){
-        switch (key) {
-            case '[CARRITO] Agregar Compra':
-                return [...state, action.payload]
-                
-            case '[CARRITO] Aumentar Cantidad':
-                
-                break;
-            case '[CARRITO] Disminuir Cantidad':
-                
-                break;
-            case '[CARRITO] Eliminar Compra':
-                return state.filter(compra => compra.id !== action.payload)
-        
-            default:
-                return state
-        }
-    }
+export const CarritoProvider = ({children}) => {
+    const [listaCompras, dispatch] = useReducer(comprasReducer, initialState);
 
-  return (
-    <CarritoContext.Provider>
-        {children}
-    </CarritoContext.Provider>
-  )
-}
+    const agregarCompra = compra => {
+        dispatch({ type: '[CARRITO] Agregar Compra', payload: compra });
+    };
+
+    const aumentarCantidad = id => {
+        dispatch({ type: '[CARRITO] Aumentar Cantidad', payload: id });
+    };
+
+    const disminuirCantidad = id => {
+        dispatch({ type: '[CARRITO] Disminuir Cantidad', payload: id });
+    };
+
+    const eliminarCompra = id => {
+        dispatch({ type: '[CARRITO] Eliminar Compra', payload: id });
+    };
+
+    return (
+        <CarritoContext.Provider value={{ listaCompras, agregarCompra, aumentarCantidad, disminuirCantidad, eliminarCompra }}>
+            {children}
+        </CarritoContext.Provider>
+    );
+};
