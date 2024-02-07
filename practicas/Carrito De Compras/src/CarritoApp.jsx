b0152cff8a8a1b4f98c0377eb474a0b5665731b7
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-07 13:08:04 -0600

    Producto provider funcionando

--- practicas/Carrito De Compras/src/CarritoApp.jsx
+++ practicas/Carrito De Compras/src/CarritoApp.jsx
@@ -2,10 +2,11 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import { NavBar } from "./components/NavBar"
 import { ComprasPage } from "./pages/ComprasPage"
 import { CarritoPage } from "./pages/CarritoPage"
+import { ProductosProvider } from "./context/ProductosProvider"
 
 export const CarritoApp = () => {
   return (
-    <>
+    <ProductosProvider>
     <NavBar></NavBar>
     <div className="container">
     <Routes>
@@ -14,6 +15,6 @@ export const CarritoApp = () => {
         <Route path="/*" element={<Navigate to="/"></Navigate>}> </Route>
     </Routes>
     </div>
-    </>
+    </ProductosProvider>
   )
 }
