7eb49b038f8849ed5c9e3ecb88e7138cc27e5775
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-16 14:28:04 -0600

    Select de perros completo

--- practicas/Dogs-API/DogsAPI.jsx
+++ practicas/Dogs-API/DogsAPI.jsx
@@ -34,7 +34,25 @@ export const DogsAPI = () => {
       >
         <option value="">Selecciona un perro</option>
         <option value="boxer">Boxer</option>
+        <option value="beagle">beagle</option>
+        <option value="doberman">doberman</option>
         <option value="labrador">Labrador</option>
+        <option value="african">african</option>
+        <option value="airedale">airedale</option>
+        <option value="akita">akita</option>
+        <option value="appenzeller">appenzeller</option>
+        <option value="basenji">basenji</option>
+        <option value="bluetick">bluetick</option>
+        <option value="borzoi">borzoi</option>
+        <option value="bouvier">bouvier</option>
+        <option value="briard">briard</option>
+        <option value="chow">chow</option>
+        <option value="clumber">clumber</option>
+        <option value="cockapoo">cockapoo</option>
+        <option value="coonhound">coonhound</option>
+        <option value="cardigan corgi">cardigan corgi</option>
+        <option value="cotondetulear">cotondetulear</option>
+        <option value="dalmatian">dalmatian</option>
       </select>
       <button
         type="button"
@@ -43,7 +61,8 @@ export const DogsAPI = () => {
       >
         Buscar
       </button>
-      {imagen && <img src={imagen} alt="Dog" />}
+      <hr />
+      {imagen && <img className="img-fluid" src={imagen} alt="Dog" />}
     </>
   );
 };
