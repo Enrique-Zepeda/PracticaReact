bf213408fe7918b85ad2a73c952adc4009d4354d
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-16 14:07:35 -0600

    Buscar dogs

--- practicas/Dogs-API/DogsAPI.jsx
+++ practicas/Dogs-API/DogsAPI.jsx
@@ -1,21 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const DogsAPI = () => {
+  const [buscar, setBuscar] = useState("");
+  const [imagen, setImagen] = useState("");
+  const [recargar, setRecargar] = useState("");
+  const URL = `https://dog.ceo/api/breed/${buscar}/images/random`;
 
-  const [Buscar, setBuscar] = useState({});
+  useEffect(() => {
+    if (buscar) {
+      fetch(URL)
+        .then((response) => response.json())
+        .then((data) => setImagen(data.message))
+        .catch((error) => console.error("Error al cargar la imagen", error));
+    }
+  }, [buscar, recargar]);
+
+  const handleInputSelect = (e) => {
+    setBuscar(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setRecargar((prev) => prev + 1);
+  };
 
   return (
     <>
       <h1>Dogs API</h1>
-      <hr className="border border-primary border-3 opacity-50"/>
+      <hr className="border border-primary border-3 opacity-50" />
       <select
         className="form-select form-select-lg mb-3"
+        value={buscar}
+        onChange={handleInputSelect}
       >
-        <option selected>Seleciona Perro</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+        <option value="">Selecciona un perro</option>
+        <option value="boxer">Boxer</option>
+        <option value="labrador">Labrador</option>
       </select>
+      <button
+        type="button"
+        className="btn btn-outline-info"
+        onClick={handleButtonClick}
+      >
+        Buscar
+      </button>
+      {imagen && <img src={imagen} alt="Dog" />}
     </>
   );
 };
