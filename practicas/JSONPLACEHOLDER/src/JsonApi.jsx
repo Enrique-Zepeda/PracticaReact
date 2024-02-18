f30803c355b975e53fe615d436563c67bb6dbc70
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-18 15:26:52 -0600

    Inicio de Practica API

---
+++ practicas/JSONPLACEHOLDER/src/JsonApi.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const JsonApi = () => {
+  return <div>JsonApi</div>;
+};
---
+++ practicas/JSONPLACEHOLDER/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { JsonApi } from "./JsonApi";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <JsonApi />
+  </React.StrictMode>
+);
