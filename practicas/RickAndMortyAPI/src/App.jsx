1889210f5b167ddfcc1e00ecc12ee1af51691ab9
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-19 13:56:13 -0600

    Mostrando los personajes con su nombre y imagen

--- practicas/RickAndMortyAPI/src/App.jsx
+++ practicas/RickAndMortyAPI/src/App.jsx
@@ -8,15 +8,28 @@ function App() {
     const fetchData = async () => {
       const response = await fetch("https://rickandmortyapi.com/api/character");
       const data = await response.json();
-      setPersonajes(data);
       console.log(data.results); //ponemos result porque la api tiene dos objeto que es info y result y nosotros solo queremos ver los results que son donde estan los personajes
+      setPersonajes(data.results); // igual aqui solo guardamos el objeto results que tiene el arreglo de personajes
     };
     fetchData();
   }, []);
 
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <h1>Rick Y Morty</h1>
+      {personajes.map((personaje) => {
+        return (
+          // aveces tenemos que poner el return porque sino no muestra nada
+          <div key={personaje.id}>
+            <p>Nombre:{personaje.name}</p>
+            <img
+              src={personaje.image}
+              alt={`Imagen del personaje:${personaje.name}`}
+            />{" "}
+            {/* Uso otras llaves porque en el return no se puede usar JS sin las llaves */}
+          </div>
+        );
+      })}
     </>
   );
 }
