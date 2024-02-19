1cc2002252c7871637dd1c50f25dcb7ed97d1f99
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-19 13:42:55 -0600

    Cambiando useState por personajes y mostrando en consola solo el objeto de results que es donde contiene los personajes

--- practicas/RickAndMortyAPI/src/App.jsx
+++ practicas/RickAndMortyAPI/src/App.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [rick, setRick] = useState([]);
+  const [personajes, setPersonajes] = useState([]);
 
   useEffect(() => {
     console.log("cargo...");
     const fetchData = async () => {
       const response = await fetch("https://rickandmortyapi.com/api/character");
       const data = await response.json();
-      setRick(data);
-      console.log(data);
+      setPersonajes(data);
+      console.log(data.results); //ponemos result porque la api tiene dos objeto que es info y result y nosotros solo queremos ver los results que son donde estan los personajes
     };
     fetchData();
   }, []);
