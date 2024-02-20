632ae036dbcb7db5dd4fc628929d6ca7502bc2b0
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-20 09:36:26 -0600

    Agregando la funcion handleSubmit para ver el title en consola

--- practicas/React-Tasks-APP/src/TaskForm.jsx
+++ practicas/React-Tasks-APP/src/TaskForm.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 
 export const TaskForm = () => {
-  const [tarea, setTarea] = useState("");
+  const [title, setTitle] = useState("");
 
   const handleInput = (e) => {
     console.log(e.target.value);
+    setTitle(e.target.value); // estoy seteando el valor que escribio el usuario
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
   };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Write a Task" onChange={handleInput} />
+      {/* Estoy enviando lo que escribe el usuario a la funcion */}
       <button>Save</button>
     </form>
   );
