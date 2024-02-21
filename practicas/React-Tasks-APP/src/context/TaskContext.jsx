007bd5773e565644da6d1494d10f63566f397cd2
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-21 08:32:12 -0600

    Creando contexto

---
+++ practicas/React-Tasks-APP/src/context/TaskContext.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+export const TaskContext = createContext();
+
+export const TaskContextProvider = (props) => {
+  return <TaskContext.Provider>{props.children}</TaskContext.Provider>;
+};
