cf124778fc3660b4924f57d5bbd4619006a8d9bc
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-22 08:33:31 -0600

    Mostrando en consola el personaje buscado

--- practicas/RickAndMortyAPI/src/components/ListadoDePersonajes.jsx
+++ practicas/RickAndMortyAPI/src/components/ListadoDePersonajes.jsx
@@ -5,6 +5,8 @@ export const Listado = () => {
   const [personajes, setPersonajes] = useState([]);
   const [loading, setLoading] = useState(true); //para crear un loading cuando la app aun no a iniciado
   const [page, setPage] = useState(1);
+  const [buscar, setBuscar] = useState("");
+  const [resultadoBusqueda, setResultadoBusqueda] = useState([]);
 
   const urlBase = "https://rickandmortyapi.com/api/character";
 
@@ -12,12 +14,26 @@ export const Listado = () => {
     const fetchData = async () => {
       const response = await fetch(`${urlBase}?page=${page}`);
       const data = await response.json();
-      console.log(data.results); //ponemos result porque la api tiene dos objeto que es info y result y nosotros solo queremos ver los results que son donde estan los personajes
+      console.log(personajes); //ponemos result porque la api tiene dos objeto que es info y result y nosotros solo queremos ver los results que son donde estan los personajes
       setLoading(false); //cuando termine de cargar los datos lo ponemos en false para que el estado cambie y cargue los personajes de la api
       setPersonajes(data.results); // igual aqui solo guardamos el objeto results que tiene el arreglo de personajes
     };
     fetchData();
   }, [page]);
+
+  const handleInput = (e) => {
+    setBuscar(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(buscar);
+    const resultados = personajes.filter((personaje) =>
+      personaje.name.toLowerCase().includes(buscar.toLowerCase())
+    );
+    setResultadoBusqueda(resultados);
+    console.log(resultados);
+  };
 
   const NavPage = () => {
     return (
@@ -50,6 +66,16 @@ export const Listado = () => {
   return (
     <div className="container">
       <NavPage />
+      <form onSubmit={handleSubmit}>
+        <input
+          className=""
+          placeholder="Escribe un personaje"
+          onChange={handleInput}
+          type="text"
+          value={buscar}
+        />
+        <button type="submit">Buscar</button>
+      </form>
       {loading ? ( //si loading es true muestras loading si no muestras los personajes
         <h1>Loading</h1>
       ) : (
@@ -63,7 +89,6 @@ export const Listado = () => {
           })}
         </div>
       )}
-      <NavPage />
     </div>
   );
 };
