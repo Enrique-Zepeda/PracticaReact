b50b689aa6673c79ff88d2fa2c64e61f3e130d5e
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-24 21:21:17 -0600

    Centrando el contenido

--- practicas/Pokemon-API/pokeApi/src/PokeAPI.jsx
+++ practicas/Pokemon-API/pokeApi/src/PokeAPI.jsx
@@ -1,11 +1,11 @@
-import { Buscador } from "./components/Buscador"
+import { Buscador } from "./components/Buscador";
 
 export const PokeAPI = () => {
   return (
-    <>
-    <h1>PokeAPI</h1>
-    <hr />
-    <Buscador></Buscador>
-    </>
-  )
-}
+    <div className="container mx-auto p-4 ">
+      <h1 className="text-center m-10 text-5xl">PokeAPI</h1>
+      <hr />
+      <Buscador></Buscador>
+    </div>
+  );
+};
--- practicas/Pokemon-API/pokeApi/src/components/Buscador.jsx
+++ practicas/Pokemon-API/pokeApi/src/components/Buscador.jsx
@@ -1,43 +1,45 @@
 import { useState } from "react";
 
 export const Buscador = () => {
+  const urlBase = "https://pokeapi.co/api/v2/pokemon/";
 
-  const urlBase = 'https://pokeapi.co/api/v2/pokemon/'
-
-  const [buscar, setBuscar] = useState('')
-  const [pokemon, setPokemon] = useState(null)
-  const [error, setError] = useState(null)
+  const [buscar, setBuscar] = useState("");
+  const [pokemon, setPokemon] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleInputChange = async (e) => {
-    setBuscar(e.target.value)
-  }
+    setBuscar(e.target.value);
+  };
 
   const handleOnSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const response = await fetch(`${urlBase}${buscar}`)
+      const response = await fetch(`${urlBase}${buscar}`);
       if (!response.ok) {
-        throw new Error('Pokémon no encontrado');
+        throw new Error("Pokémon no encontrado");
       }
-      const data = await response.json()
-      setPokemon(data)
-      setError(null)
+      const data = await response.json();
+      setPokemon(data);
+      setError(null);
     } catch (error) {
-      setError('Pokémon no encontrado');
-      console.error("Ha ocurrido un error", error)
+      setError("Pokémon no encontrado");
+      console.error("Ha ocurrido un error", error);
     }
-  }
+  };
 
   return (
-    <>
+    <div className="flex flex-col items-center">
       <form onSubmit={handleOnSubmit}>
         <input
+          className="border p-2 mr-2"
           type="text"
           placeholder="Buscar Pokemon"
           value={buscar}
           onChange={handleInputChange}
         />
-        <button type="submit">Buscar</button>
+        <button type="submit" className="bg-blue-500 text-white p-2">
+          Buscar
+        </button>
       </form>
 
       {error && <p>{error}</p>}
@@ -46,11 +48,16 @@ export const Buscador = () => {
         <div>
           <h1>{pokemon.name}</h1>
           <p>ID: {pokemon.id}</p>
-          <p>Type: {pokemon.types.map(type => type.type.name).join(', ')}</p>
-          <p>Type: {pokemon.abilities.map(ability => ability.ability.name).join(', ')}</p>
+          <p>Type: {pokemon.types.map((type) => type.type.name).join(", ")}</p>
+          <p>
+            Type:{" "}
+            {pokemon.abilities
+              .map((ability) => ability.ability.name)
+              .join(", ")}
+          </p>
           <img src={pokemon.sprites.front_default} alt="" />
         </div>
       )}
-    </>
-  )
-}
+    </div>
+  );
+};
