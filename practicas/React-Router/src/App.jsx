333e22cba350f5c1b63dcc755d4bcb915682c12a
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-26 13:48:11 -0600

    Usando routes para poder usar las url creadas y mandar a llamar los componentes

--- practicas/React-Router/src/App.jsx
+++ practicas/React-Router/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { AboutPage } from "./pages/AboutPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Hola Mundo</h1>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   );
