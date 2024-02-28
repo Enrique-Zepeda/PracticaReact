53038101ea84ea16a550592aa2bb96f379f541c2
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-27 21:33:51 -0600

    agregando isActive

--- practicas/React-Router/src/components/NavBar.jsx
+++ practicas/React-Router/src/components/NavBar.jsx
@@ -1,11 +1,16 @@
 import { NavLink } from "react-router-dom";
-
+import "./navbar.css";
 export const NavBar = () => {
   return (
     <div>
       <ul>
         <li>
-          <NavLink to="/">HomeScreen</NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "")}
+            to="/"
+          >
+            HomeScreen
+          </NavLink>
         </li>
         <li>
           <NavLink to="/about">About</NavLink>
