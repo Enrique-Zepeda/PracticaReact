cff7401f71d318ffd6e8091585b87a8cdb59eda5
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-02 22:55:12 -0600

    navbar sin estilos

--- practicas/JsonPlaceHolder_Routes/src/App.jsx
+++ practicas/JsonPlaceHolder_Routes/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UserPage } from "./pages/UserPage";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<UserPage />}></Route>
         </Routes>
