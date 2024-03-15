5cf96671df6aa84868a5b6d3dca66d399dee5aa8
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-15 12:41:16 -0600

    Mostrando datos de registro en consola

--- practicas/FireBase_Practica/src/pages/RegisterPage.jsx
+++ practicas/FireBase_Practica/src/pages/RegisterPage.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const RegisterPage = () => {
   const navigate = useNavigate();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const dataUser = Object.fromEntries(new window.FormData(event.target));
+    console.log(dataUser);
+  };
 
   const handleNavigate = () => {
     navigate("/");
   };
   return (
     <>
-      <h1>RegisterPage</h1>egisterPage
+      <h1>SignUp</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="Name" required name="name" />
+        <input type="email" placeholder="email" required name="email" />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          name="password"
+        />
+        <button type="submit">SignUp</button>
+      </form>
       <button onClick={handleNavigate}>Login</button>
     </>
   );
