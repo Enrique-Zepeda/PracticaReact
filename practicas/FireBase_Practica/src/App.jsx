3cdb3b704c7217f98ac2a39529e2640287cb4c77
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-15 13:42:46 -0600

    validando logIn

--- practicas/FireBase_Practica/src/App.jsx
+++ practicas/FireBase_Practica/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
+import { HomeScreen } from "./pages/HomeScreen";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/homeScreen" element={<HomeScreen />} />
         <Route path="*" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
--- practicas/FireBase_Practica/src/pages/LoginPage.jsx
+++ practicas/FireBase_Practica/src/pages/LoginPage.jsx
@@ -1,7 +1,29 @@
 import { useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const dataUser = Object.fromEntries(new window.FormData(event.target));
+    const { email, password } = dataUser;
+
+    const auth = getAuth();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log({ uid: user.uid, email: user.email });
+        navigate("homeScreen");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+      });
+  };
 
   const handleNavigate = () => {
     navigate("register");
@@ -10,10 +32,15 @@ export const LoginPage = () => {
   return (
     <>
       <h1>LoginPage</h1>
-      <form>
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
-        <button>Login</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" required name="email" placeholder="Email" />
+        <input
+          type="password"
+          required
+          name="password"
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
       </form>
       <button onClick={handleNavigate}>Register</button>
     </>
