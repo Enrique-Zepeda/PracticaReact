07c42fea96789a43dd23381026c5f88284dc1d51
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-17 17:43:48 -0600

    mostrando en consola descripcion y amount

--- practicas/react-expense-tracker/src/components/TransactionForm.jsx
+++ practicas/react-expense-tracker/src/components/TransactionForm.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const TransactionForm = () => {
-  return <div>TransactionForm</div>;
+  const [description, setDescription] = useState();
+  const [amount, setAmount] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(description, amount);
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Ingresa una descripcion"
+        />
+        <input
+          type="number"
+          step="0.01"
+          onChange={(e) => setAmount(e.target.value)}
+          placeholder="Ingresa un monto"
+        />
+        <button>Añade una transaccion</button>
+      </form>
+    </>
+  );
 };
