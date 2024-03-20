5eef03969ef53d562d0fba351ea7ee67120ba176
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-20 14:44:23 -0600

    Signo de dolar

--- practicas/react-expense-tracker/src/components/transactions/TransactionsItem.jsx
+++ practicas/react-expense-tracker/src/components/transactions/TransactionsItem.jsx
@@ -6,7 +6,7 @@ export const TransactionsItem = ({ transaction }) => {
   return (
     <li className="bg-zinc-600 text-white px-3 py-1 rounded-lg mb-2 w-full flex justify-between items-center">
       <p className="text-sm">{transaction.description}</p>
-      <span>{transaction.amount}</span>
+      <span>${transaction.amount}</span>
       <button onClick={() => deleteTransaction(transaction.id)}>Delete</button>
     </li>
   );
