cf17ffdc9476cbad663f28ce114fbfdcdd3c51e6
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-22 08:51:31 -0600

    Importando dependencia de menu hamburguesa

--- practicas/FireBase_Practica/src/components/HamburgerMenu.jsx
+++ practicas/FireBase_Practica/src/components/HamburgerMenu.jsx
@@ -1,7 +1,19 @@
+import { slide as Menu } from "react-burger-menu";
+
 export const HamburgerMenu = () => {
   return (
     <>
-      <h1>Menu Hamburguesa</h1>
+      <Menu>
+        <a id="home" className="menu-item" href="/">
+          Home
+        </a>
+        <a id="about" className="menu-item" href="/about">
+          About
+        </a>
+        <a id="contact" className="menu-item" href="/contact">
+          Contact
+        </a>
+      </Menu>
     </>
   );
 };
