295e4aa6e0587533a8c40a6e7cb50ed79699c719
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-26 09:35:14 -0600

    Mostrando en consola el fetch a la api

--- practicas/FireBase_Practica/src/pages/tabs/SearchMovies.jsx
+++ practicas/FireBase_Practica/src/pages/tabs/SearchMovies.jsx
@@ -3,11 +3,30 @@ import { HamburgerMenu } from "../../components/HamburgerMenu";
 
 export const SearchMovies = () => {
   const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const URL_BASE = "https://api.themoviedb.org/3/search/movie";
+  const apiKey = "f2eade83c3c75e944b3a211c852eb69c";
 
   const onSubmit = (e) => {
     e.preventDefault();
     const { busqueda } = Object.fromEntries(new window.FormData(e.target));
     console.log(busqueda);
+    setSearch(busqueda);
+    fetchPeliculas(search);
+  };
+
+  const fetchPeliculas = async () => {
+    try {
+      const response = await fetch(
+        `${URL_BASE}?query=${search}&api_key=${apiKey}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setMovies(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
