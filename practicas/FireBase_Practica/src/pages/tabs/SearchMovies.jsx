79687ec499ad32d88dfa81e5dc4850b11a2a48de
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-26 08:39:44 -0600

    Mostrando en consola el input del usuario

--- practicas/FireBase_Practica/src/pages/tabs/SearchMovies.jsx
+++ practicas/FireBase_Practica/src/pages/tabs/SearchMovies.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { HamburgerMenu } from "../../components/HamburgerMenu";
 
 export const SearchMovies = () => {
+  const [search, setSearch] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { busqueda } = Object.fromEntries(new window.FormData(e.target));
+    console.log(busqueda);
+  };
+
   return (
     <>
       <HamburgerMenu />
       <h1>Buscador Peliculas</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="Ingresa una Pelicula" name="busqueda" />
+        <button type="submit">Buscar</button>
+      </form>
     </>
   );
 };
