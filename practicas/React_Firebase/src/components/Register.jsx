a66c7ab3811d31b1d7b329e22f48628da5af7097
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-04-04 06:48:31 -0600

    Mostrando en consola el formulario con usestate

--- practicas/React_Firebase/src/components/Register.jsx
+++ practicas/React_Firebase/src/components/Register.jsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+
 export const Register = () => {
-  return <div>Register</div>;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    console.log(name, value);
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="youremail@gmail.com"
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={handleChange}
+        />
+        <button>Register</button>
+      </form>
+    </>
+  );
 };
