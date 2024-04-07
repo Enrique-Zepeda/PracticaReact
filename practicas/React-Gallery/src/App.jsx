290403810c15ed75be24d3dc1c65f58b760d70c7
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-04-06 20:18:34 -0600

    Mostrando las imagenes

--- practicas/React-Gallery/src/App.jsx
+++ practicas/React-Gallery/src/App.jsx
@@ -1,8 +1,37 @@
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+
 function App() {
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1011/1000/600/",
+      thumbnail: "https://picsum.photos/id/1011/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1010/1000/600/",
+      thumbnail: "https://picsum.photos/id/1010/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1039/1000/600/",
+      thumbnail: "https://picsum.photos/id/1039/250/150/",
+    },
+  ];
   return (
-    <>
-      <h1>Hola mundo</h1>
-    </>
+    <div>
+      <ImageGallery items={images} />
+    </div>
   );
 }
 
