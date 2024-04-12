8598e458fa64422dfcf78f85941fa56bdc74e79c
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-04-11 20:43:07 -0600

    mostrando las primeras tareas desde un estado

--- practicas/React_LocalStorage/src/App.jsx
+++ practicas/React_LocalStorage/src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import { TaskCreator } from "./components/TaskCreator";
 
 function App() {
+  const [tasksItems, setTasksItems] = useState([
+    {
+      name: "mi primer tarea",
+      done: true,
+    },
+    {
+      name: "mi segunda tarea",
+      done: false,
+    },
+    {
+      name: "mi tercera tarea",
+      done: false,
+    },
+  ]);
   return (
     <>
       <h1>Hola mundo</h1>
       <TaskCreator />
+      {tasksItems.map((task) => (
+        <div key={task.id}>{task.name}</div>
+      ))}
     </>
   );
 }
