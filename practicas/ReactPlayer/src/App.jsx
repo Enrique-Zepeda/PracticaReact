232d60bbdc2dbcbb7631501bf556f40592ab28b9
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-04-20 20:36:32 -0600

    Agregando el primer video de prueba

---
+++ practicas/ReactPlayer/src/App.jsx
@@ -0,0 +1,11 @@
+import ReactPlayer from "react-player";
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <ReactPlayer url="https://www.youtube.com/watch?v=LXb3EKWsInQ" />
+    </>
+  );
+}
+
+export default App;
