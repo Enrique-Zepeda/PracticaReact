2e4836130e894e9bbf87f2953bc622fcb4f80bdc
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-04-20 20:44:39 -0600

    Dandole controles al video y creando boton

--- practicas/ReactPlayer/src/App.jsx
+++ practicas/ReactPlayer/src/App.jsx
@@ -1,9 +1,21 @@
-import ReactPlayer from "react-player";
+import { useState } from "react";
+import YoutubePlayer from "react-player/youtube"; //para mejorar un poco el rendimiento de la app le decimos con que vamos a trabajar
+// import ReactPlayer from 'react-player'
 function App() {
+  const [playing, setPlaying] = useState(false);
+  const handleRep = () => {
+    setPlaying((prev) => !prev);
+  };
   return (
     <>
-      <h1>Hello world</h1>
-      <ReactPlayer url="https://www.youtube.com/watch?v=LXb3EKWsInQ" />
+      <button onClick={handleRep}>Parar/Reproducir</button>
+      <YoutubePlayer
+        url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+        controls
+        playing={playing}
+        // con controls le damos los controles del video dependiendo la plataforma
+      />
+      {/* <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />  */}
     </>
   );
 }
