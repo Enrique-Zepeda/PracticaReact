37a8b5984e07e97284c6182f08fc6961add80e4f
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-05-11 20:32:53 -0600

    BrowserRouter

--- practicas/RutasProtegidas/src/App.jsx
+++ practicas/RutasProtegidas/src/App.jsx
@@ -1,10 +1,11 @@
-import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <>
-      <h1>Rutas Protegidas</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<h1>Hola mundo</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
