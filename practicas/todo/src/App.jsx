f62c07cafa8495fe64b995fed48aa35c5f8919f2
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-05-21 15:02:54 -0600

    si

--- practicas/todo/src/App.jsx
+++ practicas/todo/src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  const [title, setTitle] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
       <h1>Todo App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="add new title task"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button type="submit">Add</button>
+      </form>
     </>
   );
 }
